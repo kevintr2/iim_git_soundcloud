5620b562890c7c0be7752b885741362858107f72
Kevin Tran <Kevin@MacBook-Pro-de-Kevin-3.local>
2016-09-13 00:37:50 +0200

    welcome message added

--- view/_topbar.php
+++ view/_topbar.php
@@ -6,7 +6,8 @@
 					<div class="col-xs-2 col-sm-2 col-md-1 col-lg-1 text-center">
 						<a href="index.php" class="logo"><i class="fa fa-soundcloud"></i></a>
 					</div>
-					<div class="col-xs-10 col-sm-10 col-md-11 col-lg-11">
+					<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3" style="margin-top:9px;"> <span class="welcome">Bienvenue <?php echo $_SESSION['username']; ?></span> </div>
+					<div class="col-xs-7 col-sm-7 col-md-8 col-lg-8">
 						<a href="logout.php" class="btn">Déconnexion</a>
 						<a href="edit_image.php" class="btn">Profil</a>
 						<a href="add_music.php" class="btn"><i class="fa fa-plus"></i> Ajouter une musique</a>
