696fb66405273e3d9edae9a5a7600c771ce70684
Kevin Tran <Kevin@MacBook-Pro-de-Kevin-3.local>
2016-09-13 21:16:25 +0200

    insert music style into db success

--- validation.php
+++ validation.php
@@ -3,20 +3,22 @@ require('config/config.php');
 require('model/functions.fn.php');
 session_start();
 
-if(isset($_POST['email']) && isset($_POST['username']) && isset($_POST['password'])){
-	if(!empty($_POST['email']) && !empty($_POST['username']) && !empty($_POST['password'])){
+if(isset($_POST['email']) && isset($_POST['username']) && isset($_POST['password']) && isset($_POST['musicstyle'])){
+	if(!empty($_POST['email']) && !empty($_POST['username']) && !empty($_POST['password']) && !empty($_POST['musicstyle'])){
 
 	$email = htmlspecialchars($_POST['email']);
 	$username = htmlspecialchars($_POST['username']);
 	$password = sha1($_POST['password']);
+	$musicstyle = htmlspecialchars($_POST['musicstyle']);
 
-	$req = $db->prepare("INSERT INTO users (email, username, password) VALUES (:email, :username, :password)");
+	$req = $db->prepare("INSERT INTO users (email, username, password, musicstyle) VALUES (:email, :username, :password, :musicstyle)");
 
 	$req->execute (
 		array(
 			'email' => $email,
 			'username' => $username,
-			'password' => $password
+			'password' => $password,
+			'musicstyle' => $musicstyle
 		)
 	);
 
