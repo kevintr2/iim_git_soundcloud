88d1d1d8b0d3ae48411249c59035f1ba1f8ad0cc
Kevin Tran <Kevin@MacBook-Pro-de-Kevin-3.local>
2016-09-13 23:59:00 +0200

    landing page added

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
-<?php session_start();
+<?php include 'view/_header.php'; ?>
+<body class="landing">
+		<div>
+			<a style="margin-left:70px; margin-right:10px;" class="landinglinks" href="login.php">se connecter </a> <span style="font-size:15px; color:#FFF;">|</span> <a style="margin-left:10px;" class="landinglinks" href="register.php">s'inscrire</a>
+		</div>
 
 
-if(isset($_SESSION) && !empty($_SESSION)){
-	header('Location: dashboard.php');
-}
+</body>
 
-else{
-	header('Location: login.php');
-}
+<?php include 'view/_footer.php'; ?>
