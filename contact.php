c4ce1953793261735681c0293e00a292b1fe95e9
Fabien Scalabrin <fscalabrin2@gmail.com>
2016-09-13 15:42:18 +0200

    Fix position of contact formular

--- contact.php
+++ contact.php
@@ -9,19 +9,19 @@ include 'view/dashboard.php';
 	<meta charset="utf-8">
 
 <form id="contact_form" action="#" method="POST" enctype="multipart/form-data">
-	<div class="row">
+	<div id="contactform" class="row">
 		<label for="name">Your name:</label><br />
 		<input id="name" class="input" name="name" type="text" value="" size="30" /><br />
 	</div>
-	<div class="row">
+	<div id="contactform" class="row">
 		<label for="email">Your email:</label><br />
 		<input id="email" class="input" name="email" type="text" value="" size="30" /><br />
 	</div>
-	<div class="row">
+	<div id="contactform" class="row">
 		<label for="message">Your message:</label><br />
 		<textarea id="message" class="input" name="message" rows="7" cols="30"></textarea><br />
 	</div>
-	<input id="submit_button" type="submit" value="Send email" />
+	<input id="contactform" id="submit_button" type="submit" value="Send email" />
 </form>	
 </head>
 <?php 
