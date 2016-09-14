11e2e23882a973239edc52ba82697c253af36145
Kevin Tran <kevintran@hotmail.de>
2016-09-14 16:12:26 +0200

    adding a my music page

---
+++ mymusic.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+require('config/config.php');
+require('model/functions.fn.php');
+
+
+
+include 'view/_header.php';
+include 'view/mymusic.php';
+include 'view/_footer.php';
+
+?>
