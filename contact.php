f381b5bd3325e2d3c15f1debed034db0210c4c9d
Fabien Scalabrin <fscalabrin2@gmail.com>
2016-09-14 15:19:55 +0200

    php changed for contact

--- contact.php
+++ contact.php
@@ -12,7 +12,6 @@ include 'view/_topbar.php';
 
 </head>
 <body>
-<!-- CONTENT -->
   <div id="margincontact" class="container">
   <?php if(array_key_exists('errors',$_SESSION)): ?>
   <div class="alert alert-danger">
@@ -30,53 +29,49 @@ include 'view/_topbar.php';
   <div class="form-group">
   <label for="inputname">Ton nom</label>
   <input required type="text" name="name" class="form-control" id="inputname" value="<?php echo isset($_SESSION['inputs']['name'])? $_SESSION['inputs']['name'] : ''; ?>">
-  </div><!--/*.form-group-->
-  </div><!--/*.col-md-6-->
+  </div>
+  </div>
 <div class="col-md-6">
   <div class="form-group">
   <label for="inputemail">Ton email</label>
   <input required type="email" name="email" class="form-control" id="inputemail" value="<?php echo isset($_SESSION['inputs']['email'])? $_SESSION['inputs']['email'] : ''; ?>">
-  </div><!--/*.form-group-->
-  </div><!--/*.col-md-6-->
+  </div>
+  </div>
 <div class="col-md-12">
   <div class="form-group">
   <label for="inputmessage">Ton beau message</label>
   <textarea required id="inputmessage" name="message" class="form-control"><?php echo isset($_SESSION['inputs']['message'])? $_SESSION['inputs']['message'] : ''; ?></textarea>
-  </div><!--/*.form-group-->
-  </div><!--/*.col-md-12-->
+  </div>  </div>
 <div class="col-md-12">
   <button type='submit' class='btn btn-primary'>Envoyer</button>
-  </div><!--/*.col-md-12-->
-</div><!--/*.row-->
+  </div>
+</div>
   </form>
-</div><!--/*.container-->
-  <!-- END CONTENT -->
+</div>
 </body>
   <?php
-unset($_SESSION['inputs']); // on nettoie les données précédentes
+unset($_SESSION['inputs']); 
   unset($_SESSION['success']);
   unset($_SESSION['errors']);
   ?>
 
   <?php
-session_start();//on démarre la session
-// $errors = [];
-  $errors = array(); // on crée une vérif de champs
-if(!array_key_exists('name', $_POST) || $_POST['name'] == '') {// on verifie l'existence du champ et d'un contenu
+session_start();
+  $errors = array(); 
+if(!array_key_exists('name', $_POST) || $_POST['name'] == '') {
   $errors ['name'] = "vous n'avez pas renseigné votre nom";
   }
-if(!array_key_exists('email', $_POST) || $_POST['email'] == '' || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {// on verifie existence de la clé
+if(!array_key_exists('email', $_POST) || $_POST['email'] == '' || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
   $errors ['mail'] = "vous n'avez pas renseigné votre email";
   }
 if(!array_key_exists('message', $_POST) || $_POST['message'] == '') {
   $errors ['message'] = "vous n'avez pas renseigné votre message";
   }
-if(array_key_exists('antispam', $_POST)) {// on place un petit filet anti robots spammers
+if(array_key_exists('antispam', $_POST)) {
   $errors ['antispam'] = "Vous êtes un robots spammer";
   }
-//On check les infos transmises lors de la validation
-  if(!empty($errors)){ // si erreur on renvoie vers la page précédente
-  $_SESSION['errors'] = $errors;//on stocke les erreurs
+  if(!empty($errors)){ 
+  $_SESSION['errors'] = $errors;
   $_SESSION['inputs'] = $_POST;
   header('Location: contact.php');
   }else{
@@ -84,7 +79,7 @@ if(array_key_exists('antispam', $_POST)) {// on place un petit filet anti robots
   $headers  = 'MIME-Version: 1.0' . "\r\n";
   $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
   $headers .= 'FROM:' . htmlspecialchars($_POST['email']);
-  $to = 'fscalabrin2@gmail.com'; // Insérer votre adresse email ICI
+  $to = 'fscalabrin2@gmail.com'; 
   $subject = 'Message envoyé par ' . htmlspecialchars($_POST['name']) .' - <i>' . htmlspecialchars($_POST['email']) .'</i>';
   $message_content = '
   <table>
