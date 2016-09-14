9d5d9feb47748c2ccea909dc6d32e8fb2646a68f
Fabien Scalabrin <fscalabrin2@gmail.com>
2016-09-14 16:33:50 +0200

    basic top 20 style added

--- top.php
+++ top.php
@@ -9,3 +9,12 @@ session_start();
 include 'view/_header.php';
 include 'view/_topbar.php';
 ?>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<div id="musicfeed">
+					<h1><i class="fa fa-clock-o"></i> Top des musiques du moment</h1>
+						<div class="music animated fadeInDown">
+							<div class="row">
+								<div class="col-xs-2 col-sm-2 col-md-1 col-lg-1">
