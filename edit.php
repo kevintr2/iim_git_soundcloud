0ea99cf34141768b035c744847ae0fe8ecee76fa
Kevin Tran <kevintran@hotmail.de>
2016-09-14 18:04:37 +0200

    edit music works

--- edit.php
+++ edit.php
@@ -8,6 +8,7 @@ require('model/functions.fn.php');
 /*===============================
 	Edit
 ===============================*/
+$user_id = $_SESSION['id'];
 $music_id = $_GET['id'];
 $music = selectMusic($db, $music_id);
 
@@ -15,7 +16,20 @@ if(isset($_POST) && !empty($_POST)){
 	if(!empty($_POST['title'])){
 		$edit = updateMusic($db, $_GET['id'], $_POST['title'], $_SESSION['id']);
 		if($edit == true){
-			header('Location: dashboard.php');
+
+			$title = $_POST['title'];
+			$comment = $_POST['comment'];
+
+			$req = $db->prepare("UPDATE musics SET title = :title, comment = :comment WHERE id = :id AND user_id = :user_id");
+
+			$req->execute([
+				'title' => $title,
+				'comment' => $comment,
+				'id' => $music_id,
+				'user_id' => $user_id
+				]);
+
+			header('Location: mymusic.php');
 		}
 		else{
 			$error = 'La musique ne vous appartient pas!';
