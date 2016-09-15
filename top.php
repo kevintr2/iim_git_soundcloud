33a910284e9f78b974b69e1f9ecf81c7ec9ddfe6
Fabien Scalabrin <fscalabrin2@gmail.com>
2016-09-15 14:10:11 +0200

    style top modified

--- top.php
+++ top.php
@@ -20,7 +20,7 @@ include 'view/_topbar.php';
 								<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
 								   <img src="https://i.ytimg.com/vi/9bBx_6Ih5B0/maxresdefault.jpg" class="retoucheimage">
 								       Ivan B - Lights On Me (Prod. Tido Vegas)
-											<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+											<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
 												<div id="topbar" class="clearfix">
 													<div class="container">
 														<div class="row">
@@ -47,7 +47,7 @@ include 'view/_topbar.php';
 								<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
 								   <img src="https://i.ytimg.com/vi/7kc3WUglWXo/maxresdefault.jpg" class="retoucheimage">
 								       Ivan B - Aggression (Prod. Mantra Beats)
-											<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+											<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
 												<div id="topbar" class="clearfix">
 													<div class="container">
 														<div class="row">
@@ -74,7 +74,7 @@ include 'view/_topbar.php';
 								<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
 								   <img src="http://festivus.pl/wp-content/uploads/2016/04/Woodkid-Land-of-All.jpg" class="retoucheimage">
 								       Woodkid - Land of All
-											<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+											<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
 												<div id="topbar" class="clearfix">
 													<div class="container">
 														<div class="row">
@@ -101,7 +101,7 @@ include 'view/_topbar.php';
 								<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
 								   <img src="http://www.hyconiq.com/wp-content/uploads/2016/01/980796_995434393863482_8634669492950531670_o.jpg" class="retoucheimage">
 								       Fixpen Sill - Edelweiss 
-											<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+											<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
 												<div id="topbar" class="clearfix">
 													<div class="container">
 														<div class="row">
@@ -128,7 +128,7 @@ include 'view/_topbar.php';
 								<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
 								   <img src="https://pbs.twimg.com/profile_images/696924759939026945/Z82k0hOQ.png" class="retoucheimage">
 								       Gnash - I hate u, I love u (ft. Olivia O'Brien)
-											<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+											<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
 												<div id="topbar" class="clearfix">
 													<div class="container">
 														<div class="row">
@@ -155,7 +155,7 @@ include 'view/_topbar.php';
 								<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
 								   <img src="http://images.genius.com/6620881e9a2f408e3c2d04e936bb3624.960x960x1.jpg" class="retoucheimage">
 								       $-Crew - J’aurais pas dû
-											<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+											<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
 												<div id="topbar" class="clearfix">
 													<div class="container">
 														<div class="row">
@@ -182,7 +182,7 @@ include 'view/_topbar.php';
 								<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
 								   <img src="http://media.senscritique.com/media/000014331966/source_big/Panda_Single.jpg" class="retoucheimage">
 								       Desiigner - Panda
-											<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+											<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
 												<div id="topbar" class="clearfix">
 													<div class="container">
 														<div class="row">
@@ -209,7 +209,7 @@ include 'view/_topbar.php';
 								<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
 								   <img src="http://hiphopinfosfrance.com/wp-content/uploads/2016/03/81pL3LHGiL._SL1400_.jpg" class="retoucheimage">
 								       Jazzy Bazz - 3h33
-											<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+											<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
 												<div id="topbar" class="clearfix">
 													<div class="container">
 														<div class="row">
@@ -236,7 +236,7 @@ include 'view/_topbar.php';
 								<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
 								   <img src="https://i.ytimg.com/vi/7lX8KJ3Jkos/maxresdefault.jpg" class="retoucheimage">
 								       Jazzy Bazz - 64 mesures de Spleen
-											<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+											<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
 												<div id="topbar" class="clearfix">
 													<div class="container">
 														<div class="row">
@@ -263,7 +263,7 @@ include 'view/_topbar.php';
 								<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
 								   <img src="https://i.ytimg.com/vi/F7khiq25Xr0/maxresdefault.jpg" class="retoucheimage">
 								       Joey Pecoraro - Tired Boy	
-											<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+											<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
 												<div id="topbar" class="clearfix">
 													<div class="container">
 														<div class="row">
@@ -290,7 +290,7 @@ include 'view/_topbar.php';
 								<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
 								   <img src="http://media.senscritique.com/media/000009595557/source_big/Affaire_de_famille.jpg" class="retoucheimage">
 								       MZ - Ma substance ft. Chich	
-											<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+											<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
 												<div id="topbar" class="clearfix">
 													<div class="container">
 														<div class="row">
@@ -317,7 +317,7 @@ include 'view/_topbar.php';
 								<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
 								   <img src="https://i.ytimg.com/vi/i78U3VEAwK8/maxresdefault.jpg" class="retoucheimage">
 								       Uppermost - Beautiful Light
-											<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+											<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
 												<div id="topbar" class="clearfix">
 													<div class="container">
 														<div class="row">
@@ -344,7 +344,7 @@ include 'view/_topbar.php';
 								<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
 								   <img src="https://artistxite.ca/imgcache/album/005/001/005001050_500.jpg" class="retoucheimage">
 								       Jetta - I'd Love to Change the World (Matstubs Remix)	
-											<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+											<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
 												<div id="topbar" class="clearfix">
 													<div class="container">
 														<div class="row">
@@ -371,7 +371,7 @@ include 'view/_topbar.php';
 								<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
 								   <img src="http://www.lepetitmaltais.com/wp-content/uploads/2016/08/maxresdefault.jpg" class="retoucheimage">
 								       Alexander Lewis x Brasstracks - Distant Memories	
-											<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+											<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
 												<div id="topbar" class="clearfix">
 													<div class="container">
 														<div class="row">
@@ -398,7 +398,7 @@ include 'view/_topbar.php';
 								<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
 								   <img src="https://i.ytimg.com/vi/VKzWLUQizz8/maxresdefault.jpg" class="retoucheimage">
 								       Justice - D.A.N.C.E. 
-											<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+											<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
 												<div id="topbar" class="clearfix">
 													<div class="container">
 														<div class="row">
@@ -425,7 +425,7 @@ include 'view/_topbar.php';
 								<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
 								   <img src="http://static.booska-p.com/images/news/zoom-sur-la-revelation-cousin-stizz-video-649.jpg" class="retoucheimage">
 								       Cousin Stizz - Fresh Prince	
-											<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+											<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
 												<div id="topbar" class="clearfix">
 													<div class="container">
 														<div class="row">
@@ -452,7 +452,7 @@ include 'view/_topbar.php';
 								<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
 								   <img src="https://i.ytimg.com/vi/2SaB-05BpKw/hqdefault.jpg" class="retoucheimage">
 								       Nev Plays With Himself: Zedd - Spectrum	
-											<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+											<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
 												<div id="topbar" class="clearfix">
 													<div class="container">
 														<div class="row">
@@ -479,7 +479,7 @@ include 'view/_topbar.php';
 								<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
 								   <img src="http://cdn.koreaboo.com/wp-content/uploads/2015/01/4MINUTE-Crazy-5.jpg" class="retoucheimage">
 								       4MINUTE - 미쳐(Crazy)
-											<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+											<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
 												<div id="topbar" class="clearfix">
 													<div class="container">
 														<div class="row">
@@ -506,7 +506,7 @@ include 'view/_topbar.php';
 								<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
 								   <img src="http://images.genius.com/9ce4218559f0bba27f108909f327a6a8.869x827x1.jpg" class="retoucheimage">
 								       PNL - DA
-											<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+											<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
 												<div id="topbar" class="clearfix">
 													<div class="container">
 														<div class="row">
@@ -533,7 +533,7 @@ include 'view/_topbar.php';
 								<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
 								   <img src="https://i.ytimg.com/vi/NG2IUO6bibE/maxresdefault.jpg" class="retoucheimage">
 								       Lorenzo - Freestyle Du Sale
-											<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+											<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
 												<div id="topbar" class="clearfix">
 													<div class="container">
 														<div class="row">
