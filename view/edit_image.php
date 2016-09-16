871d5d97ee18e9dd84738b6a54d7d62ce991bea9
Fabien Scalabrin <fscalabrin2@gmail.com>
2016-09-16 13:14:28 +0200

    footer added on profil page

--- view/edit_image.php
+++ view/edit_image.php
@@ -47,6 +47,24 @@
 			</div>
 		</div>
 	</div>
-<?php
-include '_footer.php';
-?>
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<div id="musicfeed">
+						<div class="music animated fadeInDown">
+							<div class="row">															      
+<footer>
+  <p class="footerme">Coded with<img src="https://freeiconshop.com/files/edd/heart-compact-solid.png" id="fbicon">                          by : Tran Kevin and Scalabrin Fabien</p>
+
+  <p class="footerme">Email Adress: <a href="mailto:someone@example.com">
+  soundcloud@gmail.com</a>.</p>
+  <a href="https://www.facebook.com/SoundCloud/?fref=ts"><img src="http://lh5.googleusercontent.com/-0MlsUFgr9nc/AAAAAAAAAAI/AAAAAAAAAFQ/A1IXqZeR9CQ/s512-c/photo.jpg" class="footerme" id="fbicon"> </a>
+  <a href="https://twitter.com/SoundCloud"><img src="https://freeiconshop.com/files/edd/twitter-solid.png" class="footerme" id="kicon"> </a>
+  
+</footer>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
